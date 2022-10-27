7640ddc32b1396c9daabae6377626fc877c2518a
zlf <1157174605@qq.com>
2022-10-27 17:48:28 +0800

    提交测试

--- netty-server/src/main/java/com/haoxy/server/NettyFutureDemo.java
+++ netty-server/src/main/java/com/haoxy/server/NettyFutureDemo.java
@@ -1,2 +1,39 @@
-package com.haoxy.server;public class test {
+package com.haoxy.server;
+
+import io.netty.util.concurrent.DefaultEventExecutorGroup;
+import io.netty.util.concurrent.EventExecutorGroup;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.FutureListener;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+
+public class NettyFutureDemo {
+    public static void main(String[] args) throws InterruptedException {
+        long l = System.currentTimeMillis();
+        EventExecutorGroup group = new DefaultEventExecutorGroup(4); // 4 threads
+        Future<Integer> f = group.submit(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                System.out.println("执行耗时操作...");
+                timeConsumingOperation();
+                return 100;
+            }
+        });
+        f.addListener(new FutureListener<Object>() {
+            @Override
+            public void operationComplete(Future<Object> objectFuture) throws Exception {
+                System.out.println("计算结果:："+objectFuture.get());
+            }
+        }).sync();
+        System.out.println("主线程运算耗时:" + (System.currentTimeMillis() - l)+" ms");
+        new CountDownLatch(1).await();//不让守护线程退出
+    }
+    static void timeConsumingOperation() {
+        try {
+            Thread.sleep(3000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
